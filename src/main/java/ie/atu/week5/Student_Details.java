a91703f7c612f664a0295b826c916c41d6ee949a
G00400519 <g00400519@atu.ie>
2022-10-25 13:38:51 +0100

    Prompt user for name, email and course

--- src/main/java/ie/atu/week5/Student_Details.java
+++ src/main/java/ie/atu/week5/Student_Details.java
@@ -8,6 +8,15 @@ public class Student_Details {
         System.out.println("Please enter your name");
         Scanner input = new Scanner(System.in);
         String name = input.next();
+
+        System.out.println("Please enter your email");
+        String email = input.next();
+
+        System.out.println("Please enter your course");
+        String course = input.next();
+
         System.out.println("Name: " + name);
+        System.out.println("Email: " + email);
+        System.out.println("Course: " + course);
     }
 }
